b57fef2defdda7e8afed3d16be5d9263b71cb9e4
Cra4zy <felix.n1@gmx.net>
2018-02-14 15:07:08 +0100

    First Commit

---
+++ SaveSync/SaveSync/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System;
+
+namespace SaveSync
+{
+    public partial class Form1 : Form
+    {
+        public string sourceDir = @Environment.ExpandEnvironmentVariables("%USERPROFILE%");
+        public string backupDir = @"c:\archives\2008";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = sourceDir;
+        }
+    }
+}
